6fc3d5551d1ff88992c1fc8cddc187b6e47e43b5
Ken Leung <mailtokenl@gmail.com>
2013-06-01 22:32:02 -0700

    kl

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject blason "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+
+  :source-paths ["src" "src/main/clojure"]
+  :java-source-paths ["src/main/java"] ; Java source is stored separately.
+  :test-paths ["test" "src/test/clojure"]
+
+  :main blason.core)
--- src/main/clojure/com/zotoh/frwk/util/coreutils.clj
+++ src/main/clojure/com/zotoh/frwk/util/coreutils.clj
@@ -22,85 +22,179 @@
   ;;(:use [ clojure.tools.logging :as LOG ])
   (:import (java.security SecureRandom))
   (:import (java.nio.charset Charset))
+  (:import (java.io File ByteArrayInputStream ByteArrayOutputStream))
   (:import (java.util Properties Date GregorianCalendar ))
   (:import (java.sql Timestamp))
-  import org.apache.commons.lang3.text.{StrSubstitutor=>STS}
-import scala.collection.mutable
-import org.slf4j._
-import org.apache.commons.lang3.{StringUtils=>STU}
-import org.apache.commons.io.FilenameUtils._
-import org.apache.commons.lang3.{SerializationUtils=>SER}
+  (:import (org.apache.commons.lang3.text StrSubstitutor))
+  (:import (org.apache.commons.lang3 StringUtils))
+  (:import (org.apache.commons.io FilenameUtils))
+  (:import (org.apache.commons.lang3 SerializationUtils))
+)
 
-  )
+(def ^:private _BOOLS #{ "true" "yes"  "on"  "ok"  "active"  "1"} )
+(defn- nsb [s]  (if (nil? s) (str "") s))
 
-(defn ^{
-    :doc "Return a new random object."
-  }
-  new-random []
-    (SecureRandom. (SecureRandom/getSeed 20)) )
+(defn new-random
+  "Return a new random object."
+  []
+  (SecureRandom. (SecureRandom/getSeed 20)) )
 
-(defn ^{
-    :doc "Return a sql Timestamp."
-  }
-  nowJTS []
-    (Timestamp. (.getTime (Date.))))
+(defn nowJTS
+  "Return a sql Timestamp."  
+  []
+  (Timestamp. (.getTime (Date.))))
 
-(defn ^{
-    :doc "Return a java Date."
-  }
-  nowJDate [] 
-    (Date. ) )
+(defn nowJDate 
+  "Return a java Date."
+  [] 
+  (Date. ) )
 
-(defn ^{
-    :doc "Return a Gregorian Calendar."
-  }
-  nowCal []
-    (GregorianCalendar. ))
+(defn nowCal
+  "Return a Gregorian Calendar."
+  []
+  (GregorianCalendar. ))
 
-
-(defn ^{
-    :doc "Return a java Charset of the encoding."
-  }
-  chSet
+(defn chSet
+  "Return a java Charset of the encoding."
   ([] (chSet "UTF-8"))
   ([enc] (Charset/forName enc)) )
 
 
-(defn ^{
-    :doc "Convert the file path into nice format without backslashes."
-  }
-  niceFPath [ fp ]
-    (if (nil? fp) (str "") (niceFPath (.getCanonicalPath fp)) ))
+(defmulti niceFPath class)
 
-(declare normalizeNoEndSeparator)
-(defn ^{
-    :doc "Convert the file path into nice format without backslashes."
-  }
-  niceFPath [fpath]
-    (normalizeNoEndSeparator fpath true) )
+(defmethod ^{ :doc "Convert the file path into nice format without backslashes." }
+  niceFPath File 
+  [ fp ]
+  (if (nil? fp) (str "") (niceFPath (.getCanonicalPath fp)) ))
 
-(defn ^{
-    :doc ""
-  }
-  filterEnvVar [ pv envs ]
-  pv)
+(defmethod ^{ :doc "Convert the file path into nice format without backslashes."  }
+  niceFPath String 
+  [fpath]
+  (FilenameUtils/normalizeNoEndSeparator (nsb fpath) true) )
 
-(defn ^{
-    :doc ""
-  }
-  filterEnvVars [ props envs ]
-    (reduce 
-      (fn [bc k]
-        (.put bc k (filterEnvVar (.get props k) envs)) bc )
-      (Properties.) (.keySet props) ))
+(defn filterVar
+  "Replaces all the variables in the given value with their matching values from the system and env vars."
+  [ value ]
+  (if (nil? value) ""
+    (.replace (StrSubstitutor. (System/getenv)) (StrSubstitutor/replaceSystemProperties value))))    
+
+(defn filterSysVar
+  "Expand any sys-var found inside the string value."
+  [value]
+  (if (nil? value) "" (StrSubstitutor/replaceSystemProperties value)) )  
+
+(defn filterEnvVar
+  "Expand any env-var found inside the string value."
+  [value]
+  (if (nil? value) "" (.replace (StrSubstitutor. (System/getenv)) value)) )    
+
+(defn filterExpandVars 
+  "Expand any env & sys vars found inside the property values."
+  [ props ]
+  (reduce 
+    (fn [bc k]
+      (.put bc k (filterVar (.get props k))) bc )
+    (Properties.) (.keySet props) ))
+
+(defn sysQuirk
+  "Get the value of a system property."
+  [prop]
+  (System/getProperty (nsb prop) ""))
+
+(defn userHomeDir
+  "Get the user's home directory."
+  []
+  (File. (sysQuirk "user.home")) )
+
+(defn userName
+  "Get the current user login name."
+  []
+  (sysQuirk "user.name"))
+
+(defn trimLastPathSep
+  "Get rid of trailing dir paths."
+  [path]  
+  (.replaceFirst (nsb path) "[/\\\\]+$"  ""))
+
+(defn serialize
+  "Object serialization."
+  [obj]
+  (if (nil? obj) nil (SerializationUtils/serialize obj)) )
+
+(defn deserialize
+  "Object deserialization."
+  [bits]
+  (if (nil? bits) nil (SerializationUtils/deserialize bits)))
+
+(defn safeGetClzname
+  "Get the object's class name."
+  [obj]
+  (if (nil? obj) "null" (.getName (.getClass obj))))
+
+(defn filePath
+  "Get the file path."
+  [fp]
+  (if (nil? fp) "" (niceFPath fp)))
+
+(defn isUnix
+  "Returns true if platform is *nix."
+  []
+  (= (.indexOf (.toLowerCase (sysQuirk "os.name")) "windows") -1 )) 
+                                                     
+(defn isWindows
+  "Returns true if platform is windows."
+  []
+  (not isUnix))
+
+(defn asLong
+  "Parse string as a long value."
+  [ s dftLongVal ]
+  (try
+    (Long/parseLong s)
+    (catch Throwable e dftLongVal)))
+
+(defn asInt
+  "Parse string as an int value."
+  [ s dftIntVal ]
+  (try
+    (Integer/parseInt s)
+    (catch Throwable e dftIntVal)))
+
+(defn asDouble
+  "Parse string as a double value."
+  [ s dftDblVal ]
+  (try
+    (Double/parseDouble s)
+    (catch Throwable e dftDblVal)))
+
+(defn asFloat
+  "Parse string as a double value."
+  [ s dftFltVal ]
+  (try
+    (Float/parseFloat s)
+    (catch Throwable e dftFltVal)))
+
+(defn asBool
+  ""
+  [s]
+  (contains? _BOOLS (.toLowerCase (nsb s))))
+  
+(defmulti asQuirks class)
+(defmethod ^{ :doc "" } 
+  asQuirks byte[]
+  [bits]
+  (asQuirks (ByteArrayInputStream. bits)))
+
+(defmethod ^{ :doc "" } 
+  asQuirks ByteArrayInputStream
+  [inp]
+  (let [ ps (Properties.) ]
+    (.load ps inp)
+    ps))
 
 
 
-(defn ^{
-    :doc ""
-  }
-  normalizeNoEndSeparator [ fpath a ]
-  nil )
+
 
 
 
