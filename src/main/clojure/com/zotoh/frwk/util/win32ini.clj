413f60b33a5dea167f4c68d21ba3ee07c04d8587
Ken Leung <mailtokenl@gmail.com>
2013-06-04 02:06:21 -0700

    kl

--- src/main/clojure/com/zotoh/frwk/util/win32ini.clj
+++ src/main/clojure/com/zotoh/frwk/util/win32ini.clj
@@ -17,101 +17,132 @@
 ;; License at
 ;; http://www.apache.org/licenses/LICENSE-2.0
 
-(ns com.zotoh.frwk.util.win32)
-
-(defprotocol IWin32Conf)
+(ns com.zotoh.frwk.util.win32
+  (:import (org.apache.commons.lang3 StringUtils))
+  (:import (java.io File IOException FileReader LineNumberReader PrintStream))
+  (:import (java.util LinkedHashMap))
+  (:import (com.zotoh.frwk.util NCMap))
+  (:require [ com.zotoh.frwk.util.coreutils :as CU ] )
+  (:require [ com.zotoh.frwk.util.strutils :as SU ] )
+  )
 
 (defn- throwBadIni [rdr]
   (throw (IOException.  (str "Bad ini line: " (.getLineNumber rdr)))))
 
-(defn- ciFind [ m k ]
-  (keys m)
-
-(defn- pSection [rdr line]
+(defn- maybeSection [rdr ncmap line]
   (let [ s (StringUtils/trim (StringUtils/strip line "[]")) ]
     (when (StringUtils/isEmpty s) (throwBadIni rdr))
+    (if-not (.containsKey ncmap s) (.put ncmap s (LinkedHashMap.)))
+    s))
 
+(defn- maybeLine [ rdr ncmap section line ]
+  (let [ kvs (.get ncmap section) ]
+    (if (nil?  kvs)
+      (throwBadIni rdr)
+      (let [ pos (.indexOf line (int \=))
+             nm (if (> pos 0) (.trim (.substring line 0 pos)) "" ) ]
+        (if (StringUtils/isEmpty nm)
+          (throwBadIni rdr)
+          (.put kvs nm (.trim (.substring line (+ pos 1))))))
+    )))
 
-            ncFind(secs, s) match {
-              case Some(x) => kvs=x
-              case None =>
-                kvs= new Section()
-                secs += Tuple2(s,kvs)
-            }
-(defn- f1 [line]
+(defn- evalOneLine [rdr ncmap line curSec]
   (let [ ln (.trim line) ]
     (cond
+      (or (StringUtils/isEmpty ln) (.startsWith ln "#")) curSec
+      (.matches ln "^\\[.*\\]$") (maybeSection rdr ncmap ln)
+      :else (do (maybeLine rdr ncmap curSec ln) curSec)
+      )))
 
-      (or (StringUtils/isEmpty ln) (.startsWith ln "#")) 
-      nil
-
-      (.matches ln "^\\[.*\\]$") (pSection ln)
-      (do
-
-     )
-
-      () ""
-
-      :else "" )
-
-(defn- ctor
-  "Parses the ini file."
+(defn- parseIniFile
   [iniFilePath]
-  (let [ rdr (LineNumberReader. (FileReader. iniFilePath)) ]
-    (loop [ line (.readLine rdr) ]
+  (let [ rdr (LineNumberReader. (FileReader. iniFilePath)) ncmap (NCMap.) ]
+    (loop [ curSec "" line (.readLine rdr)  ]
       (if (nil? line)
-        (break)
-        (let [ ln (.trim line) ]
-          (cond
-            () ""
-            () ""
-            :else "" )
+        ncmap
+        (recur (evalOneLine rdr ncmap line curSec) (.readLine rdr) )))))
+
+(defn- throwBadKey [k]
+  (throw (Exception. (str "No such property " k "."))))
+
+(defn- hasKV
+  [ m k ]
+  (if (or (nil? k) (nil? m)) nil (.containsKey m k)) )
+
+(defn- getKV [ cf s k err]
+  (let [ mp (.getSectionAsMap cf s)
+         ok (if (or (nil? mp) (nil? k)) false true ) ]
+    (if (and err (not ok))
+      (throwBadKey k)
+      (.get mp k))))
 
 
-    secs mutable.LinkedHashMap[String,Section]()
-    kvs:Section= null
+(defprotocol IWin32Conf
+  (getSectionAsMap [this sectionName] )
+  (sectionKeys [this ] )
+  (dbgShow [this])
+  (getString [this sectionName property] )
+  (getLong [this sectionName property] )
+  (getBool [this sectionName property] )
+  (getDouble [this sectionName property] ) 
+  (optString [this sectionName property] )
+  (optLong [this sectionName property] )
+  (optBool [this sectionName property] )
+  (optDouble [this sectionName property] ) ) 
 
-        ]
-    val ex= () =>
-      throw new IOException("Bad INI line: " + rdr.getLineNumber)
-    var line=""
-    var s=""
-    do {
-      line = rdr.readLine
-      if (line != null) {
-        line.trim match {
-          case ln if STU.isEmpty(ln) || ln.startsWith("#") =>
-          case ln if ln.matches("^\\[.*\\]$") =>
-            s = STU.trim( STU.strip(ln, "[]"))
-            if ( STU.isEmpty(s)) { ex }
-            ncFind(secs, s) match {
-              case Some(x) => kvs=x
-              case None =>
-                kvs= new Section()
-                secs += Tuple2(s,kvs)
-            }
-          case ln if (kvs != null) =>
-            var pos=ln.indexOf('=')
-            if (pos>0) {
-              s= ln.substring(0, pos).trim
-              if ( STU.isEmpty(s)) { ex}
-              kvs += Tuple2(s, ln.substring(pos + 1).trim )
-            } else {
-              ex
-            }
-        }
-      }
-    } while (line != null)
-    _secs = secs
-
-(defrecord Win32Conf []
-  IWin32Conf
-  ()
-
+(deftype Win32Conf [ mapOfSections ] IWin32Conf
+  (getSectionAsMap [this sectionName] (if (nil? sectionName) nil (.get mapOfSections sectionName) ))
+  (sectionKeys [this] (.keySet mapOfSections))
+  (getString [this section property]
+    (let [ v (getKV this section property true) ]
+      (SU/nsb v)))
+  (optString [this section property]
+    (let [ v (getKV this section property false) ]
+      (SU/nsb v)))
+  (getLong [this section property]
+    (let [ v (getKV this section property true) ]
+      (CU/asLong (SU/nsb v) 0)) )
+  (optLong [this section property]
+    (let [ v (getKV this section property false) ]
+      (CU/asLong (SU/nsb v) 0)) )
+  (getDouble [this section property]
+    (let [ v (getKV this section property true) ]
+      (CU/asDouble (SU/nsb v) 0.0)) )
+  (optDouble [this section property]
+    (let [ v (getKV this section property false) ]
+      (CU/asDouble (SU/nsb v) 0.0)) )
+  (getBool [this section property]
+    (let [ v (getKV this section property true) ]
+      (CU/asBool (SU/nsb v) false)) )
+  (optBool [this section property]
+    (let [ v (getKV this section property false) ]
+      (CU/asBool (SU/nsb v) false)) )
+  (dbgShow [this]
+    (doseq [ [ k v ] (seq mapOfSections) ]
+      (do
+        (println (str "[" k "]"))
+        (doseq [ [ x y ] (seq v) ]
+          (println (str x "=" y)))
+        (println))))
 
 )
-  
 
-  
 
+
+
+(defn parseConf
+  "Parse a INI config file, returning a Win32Conf object."
+  [iniFilePath]
+  (if (StringUtils/isEmpty iniFilePath)
+    nil
+    (let [ ncmap (parseIniFile iniFilePath) ]
+      (Win32Conf. ncmap))))
+
+
+
+
+
+
+
+(def ^:private win32ini-eof nil)
 
